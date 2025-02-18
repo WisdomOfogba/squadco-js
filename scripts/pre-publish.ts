ae00c5bf4300fa96cb3a6ee8669308e6f10ae017
Chiboy <chiboyekene1@gmail.com>
2025-02-19 00:56:15 +0100

    Build Script

---
+++ scripts/pre-publish.ts
@@ -0,0 +1,27 @@
+import { fileURLToPath } from "node:url";
+import { build } from 'esbuild';
+import { readFile } from "node:fs/promises";
+import { join } from "node:path";
+
+const base = (str: string) => fileURLToPath(new URL(str, import.meta.url));
+// tsc --declaration index.ts --emitDeclarationOnly --outDir ./dist
+
+// const [esmBuild, cjsBuild] =
+await Promise.allSettled([
+    build({
+        entryPoints: [base("../src/index.ts")],
+        outfile: base('../dist/esm/index.mjs'),
+        format: "esm",
+        minify: true,
+        bundle: true,
+        platform: 'node',
+    }),
+    build({
+        entryPoints: [base("../src/index.ts")],
+        outfile: base('../dist/cjs/index.cjs'),
+        format: "cjs",
+        minify: true,
+        bundle: true,
+        platform: 'node',
+    })
+])
