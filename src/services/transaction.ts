3346901c1177420b4ee78ae3dbdd10e45ec5fddf
Chiboy <chiboyekene1@gmail.com>
2025-02-19 02:14:37 +0100

    Added more features to the api

--- src/services/transaction.ts
+++ src/services/transaction.ts
@@ -1,11 +1,41 @@
+import { destr } from 'destr'
+
 import { SquadOptions } from "../types/pos";
-import { InitializePaymentError, InitializePaymentOptions, InitializePaymentReturn, InitializePaymentSuccess } from "../types/transaction";
-import { callWithHeader } from "../utils";
+import { CardChargeOptions, CardChargeReturn, GetAllTransactionsOption, GetAllTransactionsReturn, InitializePaymentError, InitializePaymentOptions, InitializePaymentReturn, InitializePaymentSuccess } from "../types/transaction";
+import { callWithHeader, checkSquadForError, formatDate, SquadError } from "../utils";
 
 export class SquadTransaction {
     options: SquadOptions
     constructor(options: SquadOptions) {
         this.options = options
+    }
+
+    async changeCard(options: CardChargeOptions) {
+
+        const response = await callWithHeader<CardChargeReturn>(this.options.secretKey, 'transaction/charge_card', {
+            method: "POST",
+            body: options,
+        })
+
+        checkSquadForError(response)
+
+        return response
+    }
+
+    async getAllTransactions(options: GetAllTransactionsOption) {
+        options.start_date = formatDate(options.start_date)!
+        options.end_date = formatDate(options.end_date)!
+
+        const response = await callWithHeader<GetAllTransactionsReturn>(this.options.secretKey, 'transaction', {
+            query: options
+        })
+
+        checkSquadForError(response)
+
+        response.data.meta_data = destr(response.data.meta_data)
+
+        return response
+
     }
 
     async initializePayment(options: InitializePaymentOptions) {
@@ -17,12 +47,24 @@ export class SquadTransaction {
             method: "POST",
             body: { ...options, pass_charge },
         })
+
+
+        const result = handleInitializeError(response)
+
+        return result
     }
 }
 
 /** Function to handle the unique return values of the **SquadTransaction.prototype.initializePayment** method */
-function handleInitializeError(response: InitializePaymentReturn): InitializePaymentSuccess {
-    if (response.status === 200) return response
+function handleInitializeError(response: InitializePaymentReturn) {
+    if (response.status === 200) return response as InitializePaymentSuccess
 
-    throw ''
+    if (response.status === 401) {
+        throw new SquadError('No API key was provided', response.status)
+    }
+
+    const { title, errors, status, traceId } = response as InitializePaymentError;
+    const errorStr = `\n${title}\ntraceId: ${traceId}\nErrors: ${JSON.stringify(errors)}`
+
+    throw new SquadError(errorStr, status)
 }
--- src/types/transaction.ts
+++ src/types/transaction.ts
@@ -1,3 +1,5 @@
+import { SharedSquadReturn } from "./shared"
+
 export type InitializePaymentOptions = {
     /** Customer's email address. */
     email: string
@@ -24,6 +26,11 @@ export type InitializePaymentOptions = {
     /** This is the ID of a merchant that was created by an aggregator which allows the aggregator initiate a transaction on behalf of the submerchant.\
         This parameter is an optional field that is passed only by a registered aggregator. */
     sub_merchant_id?: string
+    /** This allows you charge a card without collecting the card information each time (Default: *false*)
+     * 
+     *  To tokenize a card, just add this flag to the initiate payload when calling the initiate endpoint and the card will automatically be tokenized. The unique token code will automatically be added to the webhook notification that will be received after payment.
+     */
+    is_recurring?: boolean
 }
 
 export type InitializePaymentReturn = InitializePaymentSuccess | InitializePaymentError
@@ -35,6 +42,103 @@ export type InitializePaymentError = {
     errors: Record<string, string | string[]>
 }
 
-export type InitializePaymentSuccess = {
-    status: number
+
+/** SOME FIELDS WERE LEFT TO TYPE OF UNKNOWN DUE TO UNSPECIFIED PARTS OF THE DOCUMENTATION */
+export type InitializePaymentSuccess = SharedSquadReturn<{
+    auth_url: string | null,
+    access_token: string | null,
+    merchant_info: {
+        merchant_id: string
+        merchant_response: string | null,
+        merchant_name: string | null,
+        merchant_logo: string | null,
+    },
+    currency: string,
+    recurring: {
+        frequency: unknown,
+        duration: unknown,
+        type: number,
+        plan_code: unknown,
+        customer_name: string | null
+    },
+    is_recurring: boolean,
+    plan_code: unknown,
+    callback_url: string,
+    transaction_ref: string,
+    transaction_memo: string | null,
+    transaction_amount: number,
+    authorized_channels: InitializePaymentOptions['payment_channels'],
+    checkout_url: string
+}>
+
+export type CardChargeOptions = {
+    /** Amount to charge from card in the lowest currency value. kobo for NGN transactions or cent for USD transactions */
+    amount: number
+    /** A unique tokenization code for each card transaction and it is returned via the webhook for first charge on the card. */
+    token_id: string
+    /** Unique case-sensitive transaction reference. If you do not pass this parameter, Squad will generate a unique reference for you. */
+    transaction_ref?: string
 }
+
+export type CardChargeReturn = SharedSquadReturn<{
+    transaction_amount: number,
+    transaction_ref: string | null,
+    email: string | null,
+    transaction_status: unknown,
+    transaction_currency_id: unknown,
+    created_at: string,
+    transaction_type: unknown,
+    merchant_name: string | null,
+    merchant_business_name: string | null,
+    gateway_transaction_ref: string | null,
+    recurring: unknown,
+    merchant_email: string | null,
+    plan_code: unknown
+}>
+
+export type GetAllTransactionsOption = {
+    /** Transacting currency */
+    currency?: string
+    /** Transacting amount */
+    amount?: number
+    /** Number of transactions to be displayed in a page */
+    perPage?: number
+    /** Shows which page you are on */
+    page?: number
+    /** Start Date of transaction */
+    start_date: Date | string
+    /** End Date of transaction */
+    end_date: Date | string
+    /** Transaction ref of a transaction
+     * 
+     *  You are likely to use this field if you want a particular record
+     */
+    reference?: string
+}
+
+export type GetAllTransactionsReturn = SharedSquadReturn<{
+    id: number,
+    transaction_amount: number,
+    transaction_ref: string,
+    email: string,
+    merchant_id: string,
+    merchant_amount: number,
+    merchant_name: string,
+    merchant_business_name: string | null,
+    merchant_email: string | null,
+    customer_email: string | null,
+    customer_name: string | null,
+    /** This is usually a JSON payload, so by default this plugin will try to convert it to an object, and if that fails, then defaults to initial value */
+    meta_data: unknown
+    transaction_status: string,
+    transaction_charges: number,
+    /** E.g "NGN" */
+    transaction_currency_id: string,
+    transaction_gateway_id: string | null,
+    transaction_type: unknown,
+    flat_charge: number,
+    is_suspicious: boolean,
+    is_refund: boolean,
+    /** 2024-02-21T13:16:43.012+00:00 */
+    created_at: string
+}>
