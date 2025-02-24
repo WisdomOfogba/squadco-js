940869f7baf4dcbed9d7fcd79dd71aaf4281ea06
Chiboy <chiboyekene1@gmail.com>
2025-02-24 21:41:51 +0100

    Payout Service init

--- src/services/payout.ts
+++ src/services/payout.ts
@@ -1,4 +1,6 @@
+import { AccountLookupOptions, AccountLookupReturn, PayoutTransferOptions } from "../types/services/payout";
 import { SquadOptions } from "../types/services/pos";
+import { callWithHeader, checkSquadForError } from "../utils";
 
 export class SquadPayout {
     options: SquadOptions;
@@ -6,8 +8,52 @@ export class SquadPayout {
         this.options = options
     }
 
+    /** This method allows you lookup/confirm the account name of the recipient you intend to credit.\
+     *  __This should be done before initiating the transfer.__ 
+     * 
+     * Read more: https://squadinc.gitbook.io/squad-api-documentation/transfer-api#account-lookup
+    */
+    async accountLookup(options: AccountLookupOptions) {
+        const response = await callWithHeader<AccountLookupReturn>(this.options.secretKey, 'payout/account/lookup', {
+            method: "POST",
+            body: options,
+        })
 
-    async accountLookup() {
+        checkSquadForError(response)
 
+        return response
+    }
+
+    /** 
+     * This method allows you to transfer funds from your Squad Wallet to the account you have looked up.
+       Please be informed that we will not be held liable for mistake in transferring to a wrong account or an account that wasn't looked up.
+
+       ### Transaction Reference
+       Transaction Reference used to initiate a transfer must be unique per transfer. Kindly ensure that you append your merchant ID to the transaction Reference you are creating. This is compulsory as it will throw an error if you don't append it.
+
+       For instance:\
+       If my Squad Merchant ID is SBABCKDY and i want to create a transaction ref for my transfer, then I will have something like:
+
+       __"transaction_reference":"SBABCKDY_12345"__
+
+       ### ERROR CODES
+       These are the various error codes that you might get on the transfer API and the one you should re-query
+
+       200 -- Success\
+       400 --- Bad request\
+       422 --- Unprocessed\
+       424 --- Timeout/failed --- __(Should re-query)__\
+       404 --- Not found\
+       412 ---- reversed
+     */
+    async transfer(options: PayoutTransferOptions) {
+        const response = await callWithHeader<AccountLookupReturn>(this.options.secretKey, 'payout/transfer', {
+            method: "POST",
+            body: options,
+        })
+
+        checkSquadForError(response)
+
+        return response
     }
 }
---
+++ src/types/services/payout.ts
@@ -0,0 +1,43 @@
+import { SharedSquadReturn } from ".."
+
+export type AccountLookupOptions = {
+    /** Unique NIP code that identifies a bank. */
+    bank_code: string
+    /** Account number you want to transfer to */
+    account_number: string
+}
+
+export type AccountLookupReturn = SharedSquadReturn<{
+    account_name: string,
+    account_number: string
+}>
+
+export type PayoutTransferOptions = {
+    /** Unique Transaction Reference used to initiate a transfer.\
+         Please ensure that you append your merchant ID to the transaction Reference you are creating. This is compulsory as it will throw an error if you don't append it. 
+    */
+    transaction_reference: string
+    /** Amount to be transferred.\
+     *  __Value is in Kobo.__ */
+    amount: string
+    /** Unique NIP Code that identifies a bank.  */
+    bank_code: string
+    /** 10-digit NUBAN account number to be transferred to. Must be an account that has been looked up and vetted to be transferred to. */
+    account_number: string
+    /** The account name tied to the account number you are transferring to which you have looked up using our look up API. */
+    account_name: string
+    currency_id: "NGN"
+    /** A unique remark that will be sent with the transfer. */
+    remark: string
+}
+
+export type PayoutTransferReturn = SharedSquadReturn<{
+    transaction_reference: string,
+    response_description: string,
+    currency_id: "NGN",
+    amount: string,
+    nip_transaction_reference: string,
+    account_number: string,
+    account_name: string,
+    destination_institution_name: string
+}>
