65cb1dcc45367af0a71bbecb345b13ff3f8b3544
Wisdom Ofogba <wisetega007@gmail.com>
2025-02-26 00:41:17 +0100

    added sub merchant endpoint

--- src/services/merchant.ts
+++ src/services/merchant.ts
@@ -1,8 +1,22 @@
 import { SquadOptions } from "../types/services/pos";
+import { CreateSubMerchantOptions, CreateSubMerchantReturn } from "../types/services/merchant";
+import { callWithHeader, checkSquadForError } from "../utils";
 
 export class SquadMerchant {
-    options: SquadOptions
-    constructor(options: SquadOptions) {
-        this.options = options
-    }
+  options: SquadOptions;
+  constructor(options: SquadOptions) {
+    this.options = options;
+  }
+
+  async createSubMerchant(options: CreateSubMerchantOptions) {
+    // create sub merchant logic
+        const response = await callWithHeader<CreateSubMerchantReturn>(this.options.secretKey, 'merchant/create-sub-users', {
+            method: "POST",
+            body: options,
+        })
+
+        checkSquadForError(response)
+
+        return response
+  }
 }
