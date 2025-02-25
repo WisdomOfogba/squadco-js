4f50a2de7fccdd03e112a749d0b5bb28daafb7a5
Wisdom Ofogba <wisetega007@gmail.com>
2025-02-25 22:43:11 +0100

    started merchants

--- src/services/merchant.ts
+++ src/services/merchant.ts
@@ -1,7 +1,8 @@
 import { SquadOptions } from "../types/pos";
 
 export class SquadMerchant {
+    options: SquadOptions
     constructor(options: SquadOptions) {
-
+        this.options = options
     }
 }
